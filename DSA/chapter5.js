451c41d4b915ca5cf36617d5bc9d4ad85130fd2b
viditlakhera23 <lakheravidit@gmail.com>
2025-01-28 10:56:05 +0530

    merge two array dsa in js

--- DSA/chapter5.js
+++ DSA/chapter5.js
@@ -1,2 +1,15 @@
 //merge two arrays --------------------------------
-    
+let array1 = [1,2,3,4];
+let array2 = [523,4];
+
+function tomergearrays(a,b){
+    let start = 0;
+    let final = a.length + b.length
+    for( let i = a.length; i< final; i++){
+        a[i] = b[start];
+        start++
+    }
+    return a
+}
+
+console.log(tomergearrays(array1,array2));

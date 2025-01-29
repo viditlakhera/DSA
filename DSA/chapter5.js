21ba9200ac7ec7225f4a2e17b7e731ca7b3f7cdf
vidit <lakheravidit@gmail.com>
2025-01-29 22:47:02 +0530

    added third array method to merge two array code

--- DSA/chapter5.js
+++ DSA/chapter5.js
@@ -1,6 +1,6 @@
 //merge two arrays --------------------------------
 let array1 = [1,2,3,4];
-let array2 = [523,4];
+let array2 = [523,4,3,22,11,1];
 
 function tomergearrays(a,b){
     let start = 0;
@@ -12,4 +12,25 @@ function tomergearrays(a,b){
     return a
 }
 
-console.log(tomergearrays(array1,array2));
+// console.log(tomergearrays(array1,array2));
+
+
+//merge two array using third array (basically good and ideal way)---
+
+
+function thirdArray(array1, array2){
+    let array3 = [];
+
+    //using two loop
+    for( let i=0; i< array1.length; i++){
+        array3[i] = array1[i]
+    }
+
+    for( let i=0; i< array2.length; i++){
+        array3[array1.length + i] = array2[i]
+    }
+
+    return array3
+}
+
+console.log(thirdArray(array1,array2))

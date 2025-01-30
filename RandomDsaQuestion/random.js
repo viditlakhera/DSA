a728dd3272bc998c576d8b7733bb97f3cb29bd29
viditlakhera <lakheravidit@gmail.com>
2025-01-30 22:59:24 +0530

    new file added for dsa random question

---
+++ RandomDsaQuestion/random.js
@@ -0,0 +1,3 @@
+//find the sorted array is in which order asc or descending ( it should be sorted in any order first)
+let a = [55,44,22,11];
+a[0]< a[1]? console.log('ascending order'): console.log('descending order');
